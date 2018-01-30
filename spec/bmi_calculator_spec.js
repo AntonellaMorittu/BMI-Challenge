e5f0be8871c839923a0bcaabab8eacbabaa395f8
Antonella Morittu <morittuantonella@gmail.com>
2018-01-30 10:32:09 +0100

    imperial method added

--- spec/bmi_calculator_spec.js
+++ spec/bmi_calculator_spec.js
@@ -3,13 +3,18 @@ describe("BMICalculator", function() {
   var person;
 
   beforeEach(function() {
-    person = new Person({weight: 90, height: 186});
+    person_metric = new Person({weight: 90, height: 186});
+    person_imperial = new Person({weight: 40.8, height: 73.2});
     calculator = new BMICalculator();
   });
 
   it("calculates BMI for a person using metric method", function() {
     calculator.metric_bmi(person);
-    expect(person.bmiValue).toEqual(26.01);
+    expect(person_metric.bmiValue).toEqual(26.01);
   });
 
+  it("calculates BMI for a person using imperial method", function() {
+    calculator.imperial_bmi(person);
+    expect(person_imperial.bmiValue).toEqual(26.01);
+  });
 });
