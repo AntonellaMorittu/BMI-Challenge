71101a6a16adc608c7a8acbfcd5be6089b4dd828
Jade <jywfang@gmail.com>
2018-01-30 12:08:33 +0100

    imperial/metric switch WIP

--- src/person.js
+++ src/person.js
@@ -1,14 +1,20 @@
 function Person(attr) {
   this.weight = attr.weight;
   this.height = attr.height;
+  this.bmiValue = 0;
+  this.bmiMessage = '';
 }
 
-Person.prototype.calculate_bmi = function() {
-  this.bmiValue = 26.01;
-  this.bmiMessage = "Overweight";
-};
+// Person.prototype.calculate_bmi = function() {
+//   this.bmiValue = 26.01;
+//   this.bmiMessage = "Overweight";
+// };
 
-Person.prototype.calculate_bmi = function() {
+Person.prototype.calculate_bmi = function(arg) {
   calculator = new BMICalculator();
-  calculator.metric_bmi(this);
+  if (arg === 'metric') {
+    calculator.metric_bmi(this);
+  } else {
+    calculator.imperial_bmi(this);
+  }
 };
