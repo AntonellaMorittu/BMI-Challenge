e1309b360b3496a807c8680c622afa97f2a2ba07
Jade <jywfang@gmail.com>
2018-02-01 13:19:06 +0100

    buttons switch colour

---
+++ src/script.js
@@ -0,0 +1,66 @@
+$(document).ready(function () {
+  $("#weight").focus();
+});
+
+$(document).ready(function () {
+  $('#calculate').click(function () {
+    $('#display_message').html('Please input values');
+  });
+});
+$(document).ready(function () {
+  $('#imperial').click(function(){
+    $('#imperial').css('background-color', 'white');
+    $('#imperial').css('color', 'black');
+    $('#metric').css('background-color', 'transparent');
+    $('#metric').css('color', 'white');
+    $('#weight').attr('placeholder','Weight in lbs');
+    $('#height').attr('placeholder','Height in inches');
+    $('#calculate').click(function () {
+      var w = parseFloat($('#weight').val());
+      var h = parseFloat($('#height').val());
+      var person = new Person({weight: w, height: h});
+      person.calculate_bmi_imperial();
+      $('#display_value').html('Your BMI is ' + person.bmiValue);
+      $('#display_message').html('and you are '+ person.bmiMessage);
+    });
+  });
+});
+
+$(document).ready(function () {
+  $('#metric').click(function(){
+    $('#imperial').css('background-color', 'transparent');
+    $('#imperial').css('color', 'white');
+    $('#metric').css('background-color', 'white');
+    $('#metric').css('color', 'black');
+    $('#weight').attr('placeholder','Weight in kg');
+    $('#height').attr('placeholder','Height in cm');
+    $('#calculate').click(function () {
+      var w = parseFloat($('#weight').val());
+      var h = parseFloat($('#height').val());
+      var person = new Person({weight: w, height: h});
+      person.calculate_bmi_metric();
+      $('#display_value').html('Your BMI is ' + person.bmiValue);
+      $('#display_message').html('and you are '+ person.bmiMessage);
+    });
+  });
+});
+
+    // <!-- <script type="text/javascript">
+    //   $(document).ready(function () {
+    //     $('#imperial').click(function () {
+    //       var w = parseFloat($('#weight').val());
+    //       var h = parseFloat($('#height').val());
+    //       var person = new Person({weight: w, height: h});
+    //       var answer = document.getElementById('dropdown-selection').value;
+    //        if(answer === 'metric') {
+    //         person.calculate_bmi('metric');
+    //         $('#display_value').html('Your BMI is ' + person.bmiValue);
+    //         $('#display_message').html('and you are '+ person.bmiMessage);
+    //         } else {
+    //         person.calculate_bmi('imperial');
+    //         $('#display_value').html('Your BMI is ' + person.bmiValue);
+    //         $('#display_message').html('and you are '+ person.bmiMessage);
+    //        }
+    //     });
+    //   });
+    // </script> -->
